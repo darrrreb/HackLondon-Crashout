f9c1d24aeafe9f90d0102b9dfbc62106c711220f
yonyawn <jennifer.ikin@kcl.ac.uk>
2025-03-02 01:32:45 +0000

    Add repo page graph

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import './index.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import '@xyflow/react/dist/style.css';
+import './index.css';
 import App from './App.jsx';
 import LandingPage from './pages/LandingPage';
+import Flow from './pages/RepoPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/repo" element={<Flow />} />
       </Route>
     </Routes>
   </BrowserRouter>
---
+++ client/src/pages/RepoPage.jsx
@@ -0,0 +1,130 @@
+import React, { useCallback } from 'react';
+import {
+  Background,
+  ReactFlow,
+  addEdge,
+  ConnectionLineType,
+  Panel,
+  useNodesState,
+  useEdgesState, MiniMap, Controls,
+} from '@xyflow/react';
+import dagre from '@dagrejs/dagre';
+
+import '@xyflow/react/dist/style.css';
+
+import { initialNodes, initialEdges } from './initialElements.js';
+import { ArrowLeftCircleFill } from "react-bootstrap-icons";
+import { useNavigate } from "react-router-dom";
+import bookmark from "../assets/images/bookmark.png";
+
+const dagreGraph = new dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}));
+
+const nodeWidth = 172;
+const nodeHeight = 36;
+
+const getLayoutedElements = (nodes, edges, direction = 'TB') => {
+  const isHorizontal = direction === 'LR';
+  dagreGraph.setGraph({ rankdir: direction });
+
+  nodes.forEach((node) => {
+    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+  });
+
+  edges.forEach((edge) => {
+    dagreGraph.setEdge(edge.source, edge.target);
+  });
+
+  dagre.layout(dagreGraph);
+
+  const newNodes = nodes.map((node) => {
+    const nodeWithPosition = dagreGraph.node(node.id);
+    const newNode = {
+      ...node,
+      targetPosition: isHorizontal ? 'left' : 'top',
+      sourcePosition: isHorizontal ? 'right' : 'bottom',
+      position: {
+        x: nodeWithPosition.x - nodeWidth / 2,
+        y: nodeWithPosition.y - nodeHeight / 2,
+      },
+    };
+
+    return newNode;
+  });
+
+  return { nodes: newNodes, edges };
+};
+
+const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
+    initialNodes,
+    initialEdges,
+);
+
+const Flow = () => {
+  const navigate = useNavigate();
+
+  const handleNavigation = () => {
+    navigate('/');
+  };
+  const [nodes, setNodes, onNodesChange] = useNodesState(layoutedNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(layoutedEdges);
+
+  const onConnect = useCallback(
+      (params) =>
+          setEdges((eds) =>
+              addEdge(
+                  { ...params, type: ConnectionLineType.SmoothStep, animated: true },
+                  eds,
+              ),
+          ),
+      [],
+  );
+  const onLayout = useCallback(
+      (direction) => {
+        const { nodes: layoutedNodes, edges: layoutedEdges } =
+            getLayoutedElements(nodes, edges, direction);
+
+        setNodes([...layoutedNodes]);
+        setEdges([...layoutedEdges]);
+      },
+      [nodes, edges],
+  );
+
+  return (
+    <>
+      <div className="icon-repo-container">
+        <ArrowLeftCircleFill size={25} color={"white"} onClick={handleNavigation} className={"icon"}/>
+        <h3 className="main-heading">My hackathon project</h3>
+      </div>
+      <div style={{ width: 'auto', backgroundColor: '#ffdfae', borderRadius: '50px', height: '70vh' }}>
+        <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            connectionLineType={ConnectionLineType.SmoothStep}
+            fitView
+        >
+          <Panel position="top-left">
+            <img
+              alt="Bookmark"
+              src={bookmark}
+              width="100"
+              height="100"
+              className="d-inline-block"
+            />
+            <h3 style={{ marginTop: "2.25rem", marginLeft: "1.25rem", color: '#5b2323' }}>Repo Cookbook</h3>
+          </Panel>
+          <Panel position="top-right" style={{ margin: "2rem" }}>
+            <button onClick={() => onLayout('TB')}>vertical layout</button>
+            <button onClick={() => onLayout('LR')}>horizontal layout</button>
+          </Panel>
+          <MiniMap />
+          <Controls />
+        </ReactFlow>
+      </div>
+    </>
+  );
+};
+
+export default Flow;
